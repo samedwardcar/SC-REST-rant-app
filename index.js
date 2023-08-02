428cea243c46c49f3a07ec0bb9a8078e40d0fc00
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-08-01 20:01:34 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Home Page')
-})
+    res.render('home')
+    //   res.send('Hello world home page')
 
-app.get('*', (req,res) => {
+  })
+  
+
+app.get('*', (req, res) => {
     res.status(404).send('<h1 style="color: red; text-align: center;">404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
+
+
+// require('dotenv').config()
+
+// const express = require('express')
+// const app = express()
+
+
+
+// app.use('/places', require('./controllers/places'))
+
+// app.get('/', (req, res) => {
+//     res.send('Home Page')
+// })
+
+// app.get('*', (req,res) => {
+//     res.status(404).send('<h1 style="color: red; text-align: center;">404 Page</h1>')
+// })
+
+// app.listen(process.env.PORT)
