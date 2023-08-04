791c128fb861c9c479ab1b7feec2781d4b7c7011
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-08-03 22:43:53 -0500

    Adding Bootsttap grid to index page and home page. new to tweak the css for spacing on buttons.

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,49 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"/>
-                    <link rel='stylesheet' href='/css/style.css'/>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+          crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">
+                  <button type="button" className="btn btn-primary">
+                    Home
+                  </button>
+                </a>
+              </li>
+              <li>
+                <a href="/places">
+                  <button type="button" className="btn btn-primary">
+                    Places
+                  </button>
+                </a>
+              </li>
+              <li>
+                <a href="/places/new">
+                  <button type="button" className="btn btn-primary">
+                    New
+                  </button>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        
+        {html.children}
+      </body>
+    </html>
+  );
 }
 
-module.exports = Def
+module.exports = Def;
--- views/home.jsx
+++ views/home.jsx
@@ -1,30 +1,48 @@
 const React = require("react");
 const Def = require("./default");
 
-function Home () {
+function Home() {
   return (
     <Def>
       <main>
-        <h1>HOME</h1>
+        <h1>REST-Rant</h1>
         <div>
-          <img src="/images/chia-fruit-by-brenda-godinez.jpeg" alt="Chia Fruit Shake"/>
+          <img
+            src="/images/chia-fruit-by-brenda-godinez.jpeg"
+            alt="Chia Fruit Shake"
+          />
           <div>
-          Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-  
+            Photo by{" "}
+            <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Brenda Godinez
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com/photos/MsTOg6rhRVk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
           </div>
         </div>
-        <a href="/places">
-          <button type="button" className="btn btn-primary">Places Page</button>
-        </a>
-        {/* <a href="/places/show">
-          <button type="button" className="btn btn-primary">About this place</button>
-        </a>
-        <a href="/places/new">
-          <button type="button" className="btn btn-primary">New Rest-rant Form</button>
-        </a>
-        <a href="/places/edit">
-          <button type="button" className="btn btn-primary">Edit Rest-rant Info</button>
-        </a> */}
+        {/* <div>
+          <a href="/">
+            <button type="button" className="btn btn-primary">
+              Home
+            </button>
+          </a>
+        </div>
+        <div>
+          <a href="/places">
+            <button type="button" className="btn btn-primary">
+              Places
+            </button>
+          </a>
+        </div>
+        <div>
+          <a href="/places/new">
+            <button type="button" className="btn btn-primary">
+              New
+            </button>
+          </a>
+        </div> */}
       </main>
     </Def>
   );
