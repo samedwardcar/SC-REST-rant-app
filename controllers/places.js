d6da4f2c68fe7dcd76b491a04b89d82642f0d095
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-08-05 15:56:05 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,45 +1,29 @@
 const router = require('express').Router()
+const places = require('../models/places')
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
-})
-
-// breads.post('/', (req, res) => {
-//     console.log(req.body)
-//     if(req.body.hasGluten === 'on') {
-//       req.body.hasGluten = 'true'
-//     } else {
-//       req.body.hasGluten = 'false'
-//     }
-//     Bread.push(req.body)
-//     res.redirect('/breads')
-//   })
-
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'images/thai-rest-rant.jpeg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-foam-cat.jpeg'
-      }]
-          res.render('places/index', {places})
-    // res.send('GET /places')
+router.get('/new', (req, res) => {
+    res.render('places/new')
 })
 
 
