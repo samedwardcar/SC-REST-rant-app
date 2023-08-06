ac1343930240b72e4aa4439dcf0219ee774ad6ff
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-08-05 22:01:13 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,37 +1,46 @@
-const router = require('express').Router()
-const places = require('../models/places')
+const router = require("express").Router();
+const places = require("../models/places");
 
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+});
 
-router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
-  
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { places: [id] });
+  }
+});
 
-router.get('/show', (req, res) => {
-    res.render('places/show')
-})
+// router.get('/show', (req, res) => {
+//     res.render('places/show')
+// })
 
-router.get('/edit', (req, res) => {
-    res.render('places/edit')
-})
+router.get("/edit", (req, res) => {
+  res.render("places/edit");
+});
 
-module.exports = router
+module.exports = router;
