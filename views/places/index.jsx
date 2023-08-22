1e6ba1affa50ecbe83fe6f845340b63cfe5656ec
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-08-21 23:46:06 -0500

    added bonus

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -15,7 +15,7 @@ function index(data) {
         </p>
         <img src={place.pic} alt={place.name} />
         <p className="text-center">
-          Located in {place.city}, {place.state}
+          Located in {place.city}, {place.state}. Est. {place.founded}
         </p>
       </div>
     )
